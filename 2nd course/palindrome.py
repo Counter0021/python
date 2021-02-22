c3dae2a7a733182de82add31e9e39520cc4829a9
Counter0021 <ara123rus123ara@gmail.com>
2021-02-22 19:25:05 +0300

    Update

--- 2nd course/palindrome.py
+++
@@ -1,9 +0,0 @@
-def palindrom(s):
-    if (len(s) < 2):
-        return True
-    if (s[0] != s[-1]):
-        return False
-    return palindrom(s[1:-1])
-
-a = str(input("Введите слово, чтобы проверить его на палиндром: "))
-print(palindrom(a))
--- 2nd course/recursion/output_one_digit_line.py
+++
@@ -1,8 +0,0 @@
-# Вывод на каждой строке по одной цифре (рекурсия)
-def summDigit(n):
-    if (n > 0):
-        summDigit(n // 10)
-        print(n % 10)
-
-
-print(summDigit(354))
