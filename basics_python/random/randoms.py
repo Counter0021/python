b1612fde3d934145ac6b3a35b332da4da0b3c0bc
Counter0021 <ara123rus123ara@gmail.com>
2021-02-27 14:39:50 +0300

    Update

--- basics_python/random/randoms.py
+++ basics_python/random/randoms.py
@@ -1,4 +1,4 @@
-from random import random, randint, randrange
+from random import random, randint, randrange, shuffle
 
 # Рандом вещественного числа .random()
 # Чтобы число было > 0, нужно умножить его на любое целое
@@ -13,3 +13,9 @@ print(randint(0, 10))
 # Рандом числа в диапазоне без учёта конечного числа .randrange()
 # 3 аргумент - шаг(3, 6, 9)
 print(randrange(0, 10, 3))
+
+# Перемешать элементы рандомно shuffle()
+my_list = [1, 3, 56, 4]
+print(my_list)
+shuffle(my_list)
+print(my_list)
