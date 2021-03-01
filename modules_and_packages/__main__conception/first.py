9cbf12729a9e636ba16eafc4a25bf13f7ab42a96
Counter0021 <ara123rus123ara@gmail.com>
2021-03-01 16:48:09 +0300

    Studied __main__conception

---
+++ modules_and_packages/__main__conception/first.py
@@ -0,0 +1,13 @@
+def func_1():
+    print("func_1() from first.py")
+
+def func_2():
+    print("func_2() from first.py")
+
+# if (__name__ == "__main__"):
+#     print("first.py is being run directly")
+# else:
+#     print('first. py has been imported')
+
+if (__name__ == "__main__"):
+    func_1()
