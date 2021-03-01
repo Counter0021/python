26e0848a0ddb5d08d1016f8736a8e11fe89a1a45
Counter0021 <ara123rus123ara@gmail.com>
2021-03-01 15:26:02 +0300

    Studied customs and build in modules. Add directory customs_modules

---
+++ modules_and_packages/customs_modules/customs_modules.py
@@ -0,0 +1,11 @@
+# Свои модули
+import greetings as gr
+from goodbyes import say_good_bye as good_bye
+from goodbyes import say_bye_bye
+from greetings import say_hi as hi
+
+gr.say_hello()
+good_bye()
+
+hi()
+say_bye_bye()
