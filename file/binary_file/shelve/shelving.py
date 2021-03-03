9a7be4419015744a86c4c1eda66ff23a4b4d80d0
Counter0021 <ara123rus123ara@gmail.com>
2021-03-03 15:38:00 +0300

    Studied module shelve

--- file/binary_file/shelve/shelving.py
+++ file/binary_file/shelve/shelving.py
@@ -13,9 +13,44 @@ with shelve.open('shelve_test') as cars:
     print(cars['renault'])
     print(cars['mazda'])
     print(cars['opel'])
+    cars['kia'] = 'South Korea'
 
+    print(cars.get('ope'))
     # Удалить ключ
     # del cars['ope']
 
     for key in cars:
-        print(key)
+        print(key + ': ' + cars[key])
+
+    while True:
+        key = input('Please input a car name: ')
+        if key == 'quit':
+            break
+        country = cars.get(key, "We don't have a " + key)
+        print(country)
+
+    while True:
+        key = input('Please input a car name: ')
+        if key == 'quit':
+            break
+        if key in cars:
+            country = cars[key]
+            print(country)
+        else:
+            print(f"We don't have a {key}")
+
+    # Сортировка
+    ordered_keys_list = list(cars.keys())
+    ordered_keys_list.sort()
+
+    for keys in ordered_keys_list:
+        print(f"{keys} {cars[keys]}")
+
+    for value in cars.values():
+        print(value)
+
+    for keys in cars.keys():
+        print(keys)
+
+    for items in cars.items():
+        print(items)
