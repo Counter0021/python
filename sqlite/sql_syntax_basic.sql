edad6234101da7853e6ff20927fdf9b0e525117c
Counter0021 <ara123rus123ara@gmail.com>
2021-03-09 14:41:47 +0300

    SQLite

---
+++ sqlite/sql_syntax_basic.sql
@@ -0,0 +1,30 @@
+----Создать таблицы
+--CREATE TABLE students (
+--
+--	name TEXT,
+--	surname TEXT,
+--	age INTEGER
+--
+--);
+--
+--CREATE TABLE employees (
+--
+--	name TEXT,
+--	surname TEXT,
+--	age INTEGER
+--
+--);
+--
+----Сделать запрос всех данных
+--SELECT * FROM students;
+
+--Поместить данные в таблицы
+INSERT INTO employees (name, surname, age) VALUES ('Arkady', 'Counter', 25);
+INSERT INTO employees (name, surname, age) VALUES ('Daniil', 'Krost', 20);
+INSERT INTO employees (name, surname, age) VALUES ('Jack', 'Black', 25);
+INSERT INTO employees (name, surname, age) VALUES ('Jane', 'White', 20);
+
+--Удалить таблицу
+--DROP TABLE emloyees
+
+--.read - считать инфу из файла
