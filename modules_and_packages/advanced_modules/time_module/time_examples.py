8d1eaead5702c6f5a7d90a6508138f3e46db9cc5
Counter0021 <ara123rus123ara@gmail.com>
2021-03-11 14:39:56 +0300

    Studied time module

---
+++ modules_and_packages/advanced_modules/time_module/time_examples.py
@@ -0,0 +1,14 @@
+# Модуль тайм
+import time
+
+# Определение эпохи
+print(time.gmtime(0))
+
+# Текущая дата
+print(time.gmtime())
+
+# Локальное (местное время)
+print(time.localtime())
+
+# Время в секундах прошедших с начала эпохи
+print(time.time())
