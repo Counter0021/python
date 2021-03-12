bfaf5cff9435dae11ce3521c2b817ec47dd10f81
Counter0021 <ara123rus123ara@gmail.com>
2021-03-12 16:58:57 +0300

    Django

--- django_projects/my_first_project_root/my_first_project/views.py
+++ django_projects/my_first_project_root/my_first_project/views.py
@@ -1,5 +1,6 @@
 # request - пользователь отправляет запрос
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def about(request):
@@ -8,4 +9,8 @@ def about(request):
 
 def home(request):
     # Не очень
-    return HttpResponse('<h1>This page was made by: _Counter021_</h1>')
+    # return HttpResponse('<h1>This page was made by: _Counter021_</h1>')
+    # Первый параметр request обязательно
+    # Второй файл с html кодом
+    # Можно 3 - словарь (python код)
+    return render(request, 'home.html', {'greeting': 'Hello!'})
