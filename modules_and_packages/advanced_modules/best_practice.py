a2c232272952984b4f548c77508207d1caec5d22
Counter0021 <ara123rus123ara@gmail.com>
2021-03-12 15:48:43 +0300

    Practice with timezones

--- modules_and_packages/advanced_modules/best_practice.py
+++ modules_and_packages/advanced_modules/best_practice.py
@@ -6,5 +6,27 @@ import pytz
 # z - сдвиг
 iso_format_string = '%Y-%m-%dT%H:%M:%S%z'
 
+# Лучше всего делать в UTC, потом переводить в локальное время
+# Получить объект с таймзоной
+naive_now = datetime.datetime.now()
+print(naive_now)
+moscow_timezone = pytz.timezone('Europe/Moscow')
+local_datetime = moscow_timezone.localize(naive_now)
+print(local_datetime)
+
+# UTC - время
 now_utc = datetime.datetime.now(pytz.timezone('UTC'))
 print(now_utc.strftime(iso_format_string))
+
+# Время в Стамбуле
+now_eu_istanbul = now_utc.astimezone(tz=pytz.timezone('Europe/Istanbul'))
+print(now_eu_istanbul.strftime(iso_format_string))
+
+# Время в Москве
+now_eu_moscow = now_utc.astimezone(tz=pytz.timezone('Europe/Moscow'))
+print(now_eu_moscow.strftime(iso_format_string))
+
+# Все таймзоны
+all_timezones = pytz.all_timezones
+# for i in all_timezones:
+#     print(i)
